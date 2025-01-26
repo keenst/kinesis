8bd20671c09089d7bebc7f689ce6ec1cc102fe77
keenst <leo.siven@icloud.com>
2025-01-26 12:42:42 +0100

    Made sleep function cleaner

--- src/win32_time.c
+++ src/win32_time.c
@@ -13,5 +13,5 @@ double get_time_ms() {
 // TODO: Doesn't have to be in platform specific header
 void sleep_ms(const double time_ms) {
 	const double target_time = get_time_ms() + time_ms;
-	while (get_time_ms() < target_time) {}
+	while (get_time_ms() < target_time);
 }
